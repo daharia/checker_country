b119199cad7b41f4416ac71acc5af7555cae9300
daharia <fake@email.com>
2023-10-22 17:09:10 +0300

    fix

--- main.py
+++ main.py
@@ -1,51 +1,27 @@
-import json, requests, sys
+import requests, sys
 
 def check(ips: [str]) -> [str]:
     ''' Receives a list of ips, returns a list of ips with geo. '''
     ips_out = []
-    ips_prepair = [[]]
-    
+
     ips_len = len(ips)
     print('Len of list ips:', ips_len)
 
-    # Finding the middle variable for dividing a list of ips
-    ips_len += 0 if ips_len % 2 == 0 else 1 # 
-    x = 15
-    while ips_len % x != 0:
-        x += 1
-
-    # Making a new list
-    i = 0
-    j = 0
     for ip in ips:
-        if i > x:
-            j += 1
-            i = 0
-            ips_prepair.append([])
+        url = f'https://get.geojs.io/v1/ip/country?ip={ip}'
 
-        ips_prepair[j].append(ip)
-        i += 1
-    
-    datas = []
-    for ips in ips_prepair:
-        print('Ips prepair:', len(ips))
-        req = ','.join(ips)
-        url = f'https://get.geojs.io/v1/ip/country?ip={req}'
-        
         ret = requests.get(url)
         if ret.status_code != 200:
-            print(ret.status_code)
-            return ips_out
-        print(ret.text)
-        break
-        data = json.dump(ret.text)
-        datas.append(data)
+            print(ip, ret.status_code)
+            continue
+        
+        ips_out.append(ret.text.split('\n')[0])
 
     return ips_out
 
 def main():
-    if len(sys.argv) != 2:
-        print('Use: py main.py ip')
+    if len(sys.argv) != 3:
+        print('Use: python main.py input_ip.txt output_ip.txt')
         exit(1)
     
     ips = []
@@ -63,6 +39,10 @@ def main():
             ips.append(str(ip[0]))
     
     ips = check(ips)
+
+    with open(sys.argv[2], 'w') as f:
+        for ip in ips:
+            f.write(ip + '\n')
 
 if __name__ == '__main__':
     main()
